f8b4a1d8b1e208782e9d906b7dc5a6e11d3a6607
Bahman Pournazari <bahmanpn@gmail.com>
2025-06-19 05:56:16 +0200

    added blog app + post and category models to the project.but next time must define new user model in project and import it to use in post model to dont use defautl django user model.

---
+++ backend/blog_module/models.py
@@ -0,0 +1,30 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+
+class Post(models.Model):
+    ''' This is a class to define posts for blog app'''
+    author=models.ForeignKey(User,on_delete=models.CASCADE)
+    image=models.ImageField(null=True,blank=True)
+    title=models.CharField(max_length=254)
+    content=models.TextField()
+    category=models.ForeignKey('Category',on_delete=models.SET_NULL,null=True)
+    status=models.BooleanField()
+    
+    created_date=models.DateTimeField(auto_now_add=True)
+    updated_date=models.DateTimeField(auto_now=True)
+    published_date=models.DateTimeField()
+
+    def __str__(self):
+        return self.title
+
+
+class Category(models.Model):
+    ''' This is a class to define categories for posts of blog app'''
+    name=models.CharField(max_length=254)
+
+    def __str__(self):
+        return self.name
+
+
+
