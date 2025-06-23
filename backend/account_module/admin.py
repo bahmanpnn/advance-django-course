cac02098d6bfd8631e7efd1984459a14d6c17ba9
Bahman Pournazari <bahmanpn@gmail.com>
2025-06-23 13:21:26 +0200

    3.7 added add_form and form fields in custom user admin class to check and test custom user change form and custom user form for adding new user or edit user information

--- backend/account_module/admin.py
+++ backend/account_module/admin.py
@@ -1,11 +1,49 @@
+from django import forms
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth.forms import UserCreationForm,UserChangeForm
 from .models import User
 
 
+class CustomUserCreationForm(UserCreationForm):
+    class Meta:
+        model=User
+        fields=('email',)
 
-class CustoUserAdmin(UserAdmin):
+
+class CustomUserCreationFormTwo(forms.ModelForm):
+    password1=forms.CharField(label="password",widget=forms.PasswordInput)
+    password2=forms.CharField(label="password confirmation",widget=forms.PasswordInput)
+
+    class Meta:
+        model=User
+        fields=('email',)
+
+    def clean_passowrd2(self):
+        # check that the two password enrties match or not
+        password1=self.cleaned_data.get('password1')
+        password2=self.cleaned_data.get('password2')
+        if password1 and password2 and password1 != password2:
+            raise forms.ValidationError("passwords don't match!!")
+        return password2
+    def save(self, commit = True):
+        # save the provided password in hash format
+        user=super().save(commit=False)
+        user.set_password(self.cleaned_data['password1'])
+        if commit:
+            user.save()
+        return user
+
+
+class CustomUserChangeForm(forms.ModelForm):
+    pass
+
+
+class CustomUserAdmin(UserAdmin):
     model=User
+    # add_form=CustomUserCreationForm # add new user form
+    # add_form=CustomUserCreationFormTwo # add new user form
+    # form=CustomUserChangeForm # change user form
     list_display=('email','is_superuser','is_active')
     list_filter=('email','is_superuser','is_active')
     search_fields=('email',)
@@ -22,6 +60,16 @@ class CustoUserAdmin(UserAdmin):
                 'is_staff','is_superuser','is_active'
             ),
         }),
+        ('group permissions', {
+            "fields": (
+                'groups','user_permissions',
+            ),
+        }),
+        ('important user date', {
+            "fields": (
+                'last_login',
+            ),
+        }),
     )
 
     add_fieldsets=(
@@ -32,4 +80,4 @@ class CustoUserAdmin(UserAdmin):
     )
     
 
-admin.site.register(User,CustoUserAdmin)
+admin.site.register(User,CustomUserAdmin)
