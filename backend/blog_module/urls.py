9a250e05ef4a7c83788b84b7e5747f39c2227ba5
Bahman Pournazari <bahmanpn@gmail.com>
2025-06-25 13:22:55 +0200

    4.3 added some test redirect view endpoints to the blog app

--- backend/blog_module/urls.py
+++ backend/blog_module/urls.py
@@ -1,14 +1,20 @@
 from django.urls import path
 from . import views
 from django.views.generic import TemplateView
+from django.views.generic.base import RedirectView
 
-
+app_name='blog_module'
 urlpatterns = [
     path('fbv-index/',views.index,name='fbv-index'),
+
     path('templateview-index/',TemplateView.as_view(template_name="index.html",extra_context={"name":"bahman"}),name='templateview-index'),
-    
     # path('cbv-templateview-index/',views.IndexTemplateView.as_view(),name='cbv-templateview-index'),
     path('cbv-templateview-index/<str:param>',views.IndexTemplateView.as_view(),name='cbv-templateview-index'),
 
+    # redirect-view
+    # permanent false means status code 302(temporary redirecting) but default is true and status code 301(its not temporary redirecting)
+    path('go-to-google/',RedirectView.as_view(url="http://127.0.0.1:8000/blog/cbv-templateview-index/test"),name='redirectview-one'),
+    path('go-to-google-two/',RedirectView.as_view(pattern_name="blog_module:fbv-index"),name='redirectview-two'),
+    path('go-to-google-three/',RedirectView.as_view(pattern_name="blog_module:fbv-index",permanent=True),name='redirectview-three'),
     
 ]
--- backend/core/urls.py
+++ backend/core/urls.py
@@ -6,7 +6,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include("blog_module.urls")),
+    path('blog/', include("blog_module.urls",namespace='blog_module')),
 
 ]
 
