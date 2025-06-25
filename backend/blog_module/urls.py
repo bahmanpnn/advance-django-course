15354e946325a6ecee829b937012bc9ba43a9d23
Bahman Pournazari <bahmanpn@gmail.com>
2025-06-25 12:48:01 +0200

    4.2 added cbv templateview to test a class that inheritances from templateview

--- backend/blog_module/urls.py
+++ backend/blog_module/urls.py
@@ -6,4 +6,9 @@ from django.views.generic import TemplateView
 urlpatterns = [
     path('fbv-index/',views.index,name='fbv-index'),
     path('templateview-index/',TemplateView.as_view(template_name="index.html",extra_context={"name":"bahman"}),name='templateview-index'),
+    
+    # path('cbv-templateview-index/',views.IndexTemplateView.as_view(),name='cbv-templateview-index'),
+    path('cbv-templateview-index/<str:param>',views.IndexTemplateView.as_view(),name='cbv-templateview-index'),
+
+    
 ]
--- backend/blog_module/views.py
+++ backend/blog_module/views.py
@@ -1,8 +1,25 @@
 from django.shortcuts import render
-
+from django.views.generic.base import TemplateView
+from .models import Post
 
 def index(request):
     context={
         "name":"bahman pournazari"
     }
     return render(request,'index.html',context)
+
+
+class IndexTemplateView(TemplateView):
+    """
+        remember that templateview just uses for get method and request and does'nt use forms.
+        we can use it for simple page with some context with get context data method at the end!!
+    """
+
+    template_name="template_view.html"
+    def get_context_data(self, **kwargs):
+        print(self.kwargs)
+        print(self.kwargs['param'])
+        context=super().get_context_data(**kwargs)
+        context['name']="template view bahmanpn"
+        context['posts']=Post.objects.all()
+        return context
