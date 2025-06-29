738ab462a54b1f8f09c2f8edc8e446fee9ebcaa8
Bahman Pournazari <bahmanpn@gmail.com>
2025-06-29 22:29:26 +0200

    4.17 added test permissions for create view endpoint to test it

--- backend/blog_module/views.py
+++ backend/blog_module/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import get_object_or_404, redirect, render
 from django.views.generic.base import TemplateView,RedirectView
 from django.views.generic import ListView,DetailView,FormView,CreateView,UpdateView,DeleteView
-from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.mixins import LoginRequiredMixin,PermissionRequiredMixin
 from .models import Post
 from .forms import PostForm,PostCreateForm
 
@@ -90,7 +90,42 @@ class PostFormView(LoginRequiredMixin,FormView):
         return super().form_valid(form)
     
 
-class PostCreateView(LoginRequiredMixin,CreateView):
+class PostCreateView(PermissionRequiredMixin,LoginRequiredMixin,CreateView):
+    """
+        https://docs.djangoproject.com/en/5.2/topics/auth/default/
+        https://django-tutelary.readthedocs.io/en/latest/usage/permissions-for-views.html
+
+        myuser.groups.set([group_list])
+        myuser.groups.add(group, group, ...)
+        myuser.groups.remove(group, group, ...)
+        myuser.groups.clear()
+        myuser.user_permissions.set([permission_list])
+        myuser.user_permissions.add(permission, permission, ...)
+        myuser.user_permissions.remove(permission, permission, ...)
+        myuser.user_permissions.clear()
+        
+        #####
+        add: user.has_perm('foo.add_bar')
+
+        change: user.has_perm('foo.change_bar')
+
+        delete: user.has_perm('foo.delete_bar')
+
+        view: user.has_perm('foo.view_bar')
+
+        permission mixin give us a permission required and raise_exception attrs to add permissions that we want for this view and endpoint.
+        we can override has_permission method of class too.
+        def has_permission(self):
+            user = self.request.user
+            return user.has_perm('blog_module.can_open') or user.has_perm('blog_module.can_edit')
+    """
+    # permission_required=("blog_module.change_post","blog_module.add_post","blog_module.delete_post","blog_module.get_post")
+    # permission_required = {
+    #   'GET': None,
+    #   'POST': 'board.create'
+    # }
+    permission_required="blog_module.add_post"
+    raise_exception = True
 
     model=Post
     form_class=PostCreateForm # we can use fields attr instead of formclass too,but fields styling handleling is too difficult.
@@ -101,7 +136,7 @@ class PostCreateView(LoginRequiredMixin,CreateView):
         form.instance.author=self.request.user
         form.save()
         return super().form_valid(form)
-
+    
 
 class PostUpdateView(LoginRequiredMixin,UpdateView):
     template_name="post_create_form.html"
