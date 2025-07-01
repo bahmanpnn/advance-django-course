a48e34c866ef5366a734ec822cb856cce7c8247f
Bahman Pournazari <bahmanpn@gmail.com>
2025-07-01 17:28:28 +0200

    5.9 post detail api view updated and added status code for it and handled wrong post detail id that get from url with 404 status code in response in 2 examples

--- backend/blog_module/api/v1/views.py
+++ backend/blog_module/api/v1/views.py
@@ -1,25 +1,29 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from rest_framework import status
+from django.shortcuts import get_object_or_404
 from .serializers import PostSerializer
 from ...models import Post # from blog_module.models import Post
+
 
 # Django Rest Framework v1 Endpoints.
 @api_view()
 def post_list_api_view(request):
     return Response("post list api view")
 
-
 # @api_view()
 # def post_detail_api_view(request,id):
-#     data={
-#         "name":"bahman",
-#         "age":26,
-#         "id":id
-#     }
-#     return Response(data)
+#     try:
+#         post=Post.objects.get(pk=id)
+#         serializer=PostSerializer(post)
+#         return Response(serializer.data)
+#     except Post.DoesNotExist:
+#         # remember that structure of sending data for response is a dictionary,
+#         # so we have to send values and args with dictionary like detail in this example.
+#         return Response({"detail":"post does'nt exists!! try again"},status=status.HTTP_404_NOT_FOUND)
 
 @api_view()
 def post_detail_api_view(request,id):
-    post=Post.objects.get(pk=id)
+    post=get_object_or_404(Post,pk=id)
     serializer=PostSerializer(post)
     return Response(serializer.data)
