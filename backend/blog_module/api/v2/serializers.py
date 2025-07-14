8ca63bb3112e979725fcc26baf3036322a43be56
Bahman Pournazari <bahmanpn@gmail.com>
2025-07-14 15:43:42 +0200

    6.18 added a related field(category) with serializer slug related field to use name of category instead of default id that server and show in post serializer for requests(get,post,...)

--- backend/blog_module/api/v2/serializers.py
+++ backend/blog_module/api/v2/serializers.py
@@ -8,6 +8,12 @@ from ...models import Post,Category
 #     id=serializers.IntegerField()
 #     title=serializers.CharField(max_length=255)
 
+class CategorySerializer(serializers.ModelSerializer):
+    class Meta:
+        model=Category
+        fields="__all__"
+        # fields=["id",""]
+    
 
 class PostSerializer(serializers.ModelSerializer):
     """there is three type of readonly field"""
@@ -19,7 +25,10 @@ class PostSerializer(serializers.ModelSerializer):
     snippet=serializers.ReadOnlyField(source='get_snippet')
     relative_url=serializers.URLField(source='get_absolute_api_url',read_only=True)
     abs_url=serializers.SerializerMethodField(method_name='get_abs_url') # with this field django search method in serializer.if dont set method name it search by default (get_)+ field name.
-
+    
+    # category=CategorySerializer()
+    # category=serializers.SlugRelatedField(many=False,read_only=True,slug_field='name')
+    category=serializers.SlugRelatedField(many=False,read_only=False,slug_field='name',queryset=Category.objects.all())
 
     class Meta:
         model=Post
@@ -37,11 +46,7 @@ class PostSerializer(serializers.ModelSerializer):
         req = self.context.get("request")
         return req.build_absolute_uri(obj.pk)
 
+    # def to_representation(self, instance):
+    #     return super().to_representation(instance)
 
 
-class CategorySerializer(serializers.ModelSerializer):
-    class Meta:
-        model=Category
-        fields="__all__"
-        # fields=["id",""]
-    
