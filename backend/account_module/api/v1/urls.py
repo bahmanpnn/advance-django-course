3e7a9e5f6b179c76f5a48256b6cca8f747ed786f
Bahman Pournazari <bahmanpn@gmail.com>
2025-07-21 13:31:34 +0200

    8.7+8.8 added obtain auth token+custom auth token to the project to test token and endpoints and know how add extra to custom obtain token like destroying or something

--- backend/account_module/api/v1/urls.py
+++ backend/account_module/api/v1/urls.py
@@ -1,20 +1,24 @@
 from django.urls import path,include
 from rest_framework.routers import DefaultRouter,SimpleRouter
+from rest_framework.authtoken.views import ObtainAuthToken
 from . import views
+
 
 app_name="api-v1"
 
 urlpatterns = [
-    # http://127.0.0.1:8000/accounts/api/v1//
+    # http://127.0.0.1:8000/accounts/api/v1/
 
     # registration
-    path('registration/',views.RegistrationApiView.as_view(),name="registeration")
+    path('registration/',views.RegistrationApiView.as_view(),name="registeration"),
 
     # change password
 
     # reset password
 
     # login token
+    path('token/login/',ObtainAuthToken.as_view(),name="token-login"),
+    path('token/custom-login/',views.CustomObtainAuthToken.as_view(),name="custom-token-login"),
 
     # login jwt
 
