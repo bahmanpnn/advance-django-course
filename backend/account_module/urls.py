47f40ea4618348a5f75276e12ae25ef30c65d326
Bahman Pournazari <bahmanpn@gmail.com>
2025-07-30 20:18:43 +0200

    8.32 added djoser package as a version 2 of account module api endpoints and authentication

--- backend/account_module/urls.py
+++ backend/account_module/urls.py
@@ -5,4 +5,6 @@ app_name='account_module'
 urlpatterns = [
     path('', include("django.contrib.auth.urls")),
     path('api/v1/',include("account_module.api.v1.urls")),
+    path('api/v2/',include("djoser.urls")),
+    path('api/v2/',include("djoser.urls.jwt")),
 ]
--- backend/core/settings.py
+++ backend/core/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'drf_yasg', # for swagger+redoc and api document
     'django_filters', # for drf filters(search,filtering,ordering,...)
     'mail_templated',
+    'djoser',
     
 ]
 
