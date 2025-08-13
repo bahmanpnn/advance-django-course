a2af39f06e9659acbd6d203a0189f3a1ec394b8f
Bahman Pournazari <bahmanpn@gmail.com>
2025-08-13 05:59:44 +0200

    12.3 updated command class. added constructor method + handle method updated and now it create new random user.next time i update it and for new user faker add some posts auto with command

--- backend/polls/management/commands/insert_data.py
+++ backend/polls/management/commands/insert_data.py
@@ -1,5 +1,6 @@
 from django.core.management import BaseCommand
 from faker import Faker
+from account_module.models import User,Profile
 
 
 
@@ -7,10 +8,16 @@ from faker import Faker
 class Command(BaseCommand):
     help="inserting dummy data to tables"
 
+    def __init__(self,*args, **kwargs):
+        super(Command,self).__init__(*args, **kwargs)
+        self.fake=Faker() # we do this because just need one object from Faker not many but this Object can every time give us random data for everything!!
+
     def handle(self, *args, **options):
-        fake=Faker()
-        print(fake.name())
-        print(fake.job())
-        print(fake.email())
+        user=User.objects.create_user(email=self.fake.email(),password="Test12345!@")
+        profile=Profile.objects.get(user=user)
+        profile.first_name=self.fake.first_name()
+        profile.last_name=self.fake.last_name()
+        profile.description=self.fake.paragraph(nb_sentences=5)
+        profile.save()
 
 # now just rund this file in terminal ==> python manage.py insert_data
