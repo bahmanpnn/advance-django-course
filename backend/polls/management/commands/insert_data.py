faaa25c95ee16cb2161b3c3fb9a971b5ea179d6b
Bahman Pournazari <bahmanpn@gmail.com>
2025-08-13 06:20:57 +0200

    12.4 insert data file updated and added category list to python every time use diffrent category in posts.now after creating user and profile django will add some posts for that user.just next time focus for adding fake image too(from faker or add static image to use for command and automation.

--- backend/polls/management/commands/insert_data.py
+++ backend/polls/management/commands/insert_data.py
@@ -1,8 +1,14 @@
-from django.core.management import BaseCommand
 from faker import Faker
+import random
+from datetime import datetime
+from django.core.management import BaseCommand
+from blog_module.models import Post,Category
 from account_module.models import User,Profile
 
 
+category_list=[
+    "IT","Design","Fun"
+]
 
 # remember that we must dont change class name(Command) because django cant find command class!!
 class Command(BaseCommand):
@@ -13,11 +19,26 @@ class Command(BaseCommand):
         self.fake=Faker() # we do this because just need one object from Faker not many but this Object can every time give us random data for everything!!
 
     def handle(self, *args, **options):
+        
         user=User.objects.create_user(email=self.fake.email(),password="Test12345!@")
         profile=Profile.objects.get(user=user)
         profile.first_name=self.fake.first_name()
         profile.last_name=self.fake.last_name()
         profile.description=self.fake.paragraph(nb_sentences=5)
         profile.save()
+
+        for name in category_list:
+            Category.objects.get_or_create(name=name)
+        
+        for _ in range(5):
+            Post.objects.create(
+                author=profile,
+                title=self.fake.paragraph(nb_sentences=1),
+                content=self.fake.paragraph(nb_sentences=10),
+                status=random.choice([True,False]),
+                category=Category.objects.get(name=random.choice(category_list)),
+                published_date=datetime.now(),
+                # image="images/blank.jpg",
+            )
 
 # now just rund this file in terminal ==> python manage.py insert_data
